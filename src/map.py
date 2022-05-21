2b6c72074464a494dd3afcd21279fde81f801348
Piotrek <piotr.kica.35@gmail.com>
2022-05-21 14:34:00 +0200

    move calculate circle intersection method

--- src/map.py
+++ src/map.py
@@ -6,6 +6,8 @@ from src.antenna import Antenna
 from src.city import City
 import numpy as np
 
+from src.utils import circles_intersection_area
+
 
 class Map:
     def __init__(self, height, width, cities: [City], antennas: [Antenna]):
@@ -15,26 +17,6 @@ class Map:
         self.antennas = antennas
 
     def calculate_cost(self, solution):
-        def circles_intersection_area(circle_a, circle_b):
-            x_a, y_a, r_a = circle_a
-            x_b, y_b, r_b = circle_b
-            distance = np.linalg.norm(np.array([x_a, y_a] - np.array([x_b, y_b])))
-            if distance > r_a + r_b:
-                return 0
-
-            R = max(r_a, r_b)
-            r = min(r_a, r_b)
-
-            if R - r - distance > 0:
-                return np.pi * r * r
-
-            part1 = r * r * np.arccos((distance * distance + r * r - R * R) / (2 * distance * r))
-            part2 = R * R * np.arccos((distance * distance + R * R - r * r) / (2 * distance * R))
-            part3 = 0.5 * np.sqrt((-distance + r + R) * (distance + r - R) * (distance - r + R) * (distance + r + R))
-
-            intersection_area = part1 + part2 - part3
-            return intersection_area
-
         total_cost = 0
         for antenna_type, positions in solution.items():
             for pos in positions:
